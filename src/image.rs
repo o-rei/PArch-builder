2c7752ea215ff9a869fc74b570599a57fd710c56
Matt Turner <maturner01@gmail.com>
2026-09-17 22:00:08 -0700

    final alignment finished for imager

--- src/image.rs
+++ src/image.rs
@@ -1,23 +1,31 @@
-use std::path::{Path,PathBuf};
+use std::{
+    fs::File,
+    path::{Path, PathBuf},
+};
 
 pub fn create_image(
         sbc_model: &str,
         target_drive: impl AsRef<Path>
-    ) -> anyhow::Result<PathBuf> { // , Box<dyn std::error::Error>> {
+    ) -> anyhow::Result<PathBuf> {
 
     // XXX Nonsense line below to get it to compile nicely but do nada
     let target_drive = target_drive.as_ref().join(sbc_model);
     Ok(target_drive)
 }
 
+#[cfg(target_os = "linux")]
+pub fn makevirtsd(
+        image_path: impl AsRef<Path>,
+        size_mib: u64
+    ) -> anyhow::Result<PathBuf> {
 
-pub fn makevirtsd() -> anyhow::Result<PathBuf> {
+    // Extract the reference from generic and create a new image file
+    let image_path = image_path.as_ref();
+    let image = File::create(image_path)?;
+    // Set the logical size of the image file
+    image.set_len(size_mib * 1024 * 1024)?;
 
-    // This one needs to be constructed bc right now there's no
-    // inputs with type information: target_drive converted by
-    // `as_ref` is a Path, then a Path can easily be converted to
-    // a PathBuf?
-    Ok(PathBuf::from(""))
+    Ok(image_path.to_path_buf())
 }
 
 
@@ -26,20 +34,21 @@ pub fn makevirtsd() -> anyhow::Result<PathBuf> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::path::PathBuf;
-
 
     #[cfg(target_os = "linux")]
     #[test]
     fn mock_filesystem_image_created() -> anyhow::Result<()> {
-        let dir = tempdir()?;
+        let dir = tempfile::tempdir()?;
         let image_path = dir.path().join("virtsd.img");
 
-        let device_path = makevirtsd(&image_path, 512)?;
-        // let virtsd: PathBuf = PathBuf::from("");
+        let created_path = makevirtsd(&image_path, 512)?;
 
-        // nonsense expected not to pass but compiles nicely
-        // assert!(device_path == virtsd);
+        assert_eq!(created_path, image_path);
+        assert!(created_path.exists());
+        assert_eq!(
+            std::fs::metadata(&created_path)?.len(),
+            512 * 1024 * 1024
+        );
 
         Ok(())
     }
